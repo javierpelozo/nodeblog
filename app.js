212d184e93f0bb94797d9533ab69b3a8fd7d4d39
javierpelozo <javpelozo@gmail.com>
2016-09-23 22:22:20 -0400

    second commit

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 //Subir archivos
-app.use(multer({ dest: './public/images/uploads'}));
+app.use(multer({ dest: './public/images/uploads' }));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
